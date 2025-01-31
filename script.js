d11df7c3fd45b24c6611ee352a6d94c570ffece2
Dylan Cocoletzi <dcocoletzi49@gmail.com>
2025-01-30 20:04:47 -0800

    Changed the function to return an object instead of an array

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const stringArray = userInputString.split(",");
 console.log("Array of inputs: ", stringArray);
 
 function countFlavors(arr){
-    let obj = []
+    let obj = {}
     for(let i = 0; i < arr.length; i++){
         if(arr[i] in obj){
             obj[arr[i]] = obj[arr[i]] + 1;
